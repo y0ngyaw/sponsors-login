9c9b70cd7fa4a7ea18a835b35129f096316fe393
y0ngyaw <yongyawyeo98@gmail.com>
2017-12-29 00:34:20 +0800

    Update UI

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,17 +1,23 @@
-$(document).ready(function(){
-	//$('.row-box').each(slideIn());
-	/*var all_row_box = document.querySelectorAll('#row-box');
-	var timeout_counter = 1000;
-	$('.row-box').animate({'opacity': 1}, 1500);
-	for(i=0; i<all_row_box.length; i++){
-		setTimeout(slideIn(all_row_box[i]), 10000);
-		console.log(i);
-		timeout_counter = timeout_counter + 1000;
-	};
-	perform();*/
-	/*var row_box = $('.row-box');
-	row_box.addClass('slide-in');*/
+$(document).on("turbolinks:load", function(){
+	slideInExecution();
 
+	$('.button_to').mouseenter(function(){
+		$(this).addClass('hover');
+	});
+	$('.button_to').mouseleave(function(){
+		$(this).removeClass('hover');
+	});
+
+	$('.nav-selection').mouseenter(function(){
+		$(this).addClass('hover');
+	});
+	/*$('.nav-selection').mouseleave(function(){
+		$(this).removeClass('hover');
+	});*/
+});
+
+
+function slideInExecution(){
 	var timeout_counter = 0;
 	var array = [].slice.call(document.querySelectorAll("#row-box"));
 	for(i=0; i<array.length; i++){
@@ -20,10 +26,8 @@ $(document).ready(function(){
 		slideIn(array[i]);
 		$(array[i]).css("animation", value);
 		timeout_counter = timeout_counter + 0.1;
-	}
-
-});
-
+	};
+}
 
 function slideIn(object){
 	var temp_object = $(object);
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,6 +20,7 @@ class ProjectsController < ApplicationController
 	def show
 		@project = Project.find(params[:id])
 		respond_to do |format|
+			format.html;
 			format.js
 		end 
 	end 
